04a3f3e8c7f1be8a5dc207822666b8a2aae9e6de
mluk <mikauschkal@gmail.com>
2022-01-29 14:15:41 +0100

    sqlalchemy docs

--- utils.py
+++ utils.py
@@ -1,10 +1,15 @@
-import pprint
-
+import pandas as pd
 import sqlalchemy
 
 
 def print_table(engine, table_name: str):
     with engine.connect() as c:
-        r = c.execute(sqlalchemy.text(f"Select * from {table_name}"))
-        t_rows = r.fetchall()
-        pprint.pprint(t_rows)
+        # fetch all rows & pipe into pandas:
+        res = c.execute(sqlalchemy.text(f"Select * from {table_name}"))
+        rows = res.fetchall()
+        # use reflection in SqlAlchemy for getting columns of table:
+        inspector = sqlalchemy.inspect(engine)
+        columns = [col.get("name") for col in inspector.get_columns(table_name)]
+        df = pd.DataFrame.from_records(rows, columns=columns)
+        print(f"************ Table {table_name} ************".upper())
+        print(df)
