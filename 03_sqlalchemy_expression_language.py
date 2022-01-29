d68cee72a29efadaf3fdfe170a01bac718511a5b
mluk <mikauschkal@gmail.com>
2022-01-29 21:55:18 +0100

    sqlalchemy query language

--- 03_sqlalchemy_expression_language.py
+++ 03_sqlalchemy_expression_language.py
@@ -1,4 +1,7 @@
+import pandas as pd
 import sqlalchemy
+
+import utils
 
 # SqlAlchemy Query Language:
 # - is built on MetaData
@@ -10,15 +13,31 @@ metadata = sqlalchemy.MetaData()
 user_table = sqlalchemy.Table(
     "user",
     metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer(), primary_key=True),
+    sqlalchemy.Column("id", sqlalchemy.Integer(), primary_key=True, autoincrement=True),
     sqlalchemy.Column("first_name", sqlalchemy.String(), nullable=False),
     sqlalchemy.Column("last_name", sqlalchemy.String(), nullable=False),
     sqlalchemy.Column("age", sqlalchemy.Integer(), nullable=False),
 )
 
 # Create Engine:
-engine = sqlalchemy.create_engine("sqlite+sqlite2:///:memory:")
+engine = sqlalchemy.create_engine("sqlite:///:memory:")
 
 # Create connection, start transaction and create Tables:
 with engine.begin() as con:
     metadata.create_all(con)
+    print(f"* Inserting following Csv data...")
+    df = pd.read_csv("user.csv", header=0)
+    print(df)
+    df.to_sql(
+        # target table name:
+        "user",
+        con,
+        # ignore index i.e. no extra column just for the index
+        index=False,
+        # "append" to existing table
+        # alternatives:
+        # "fail", if table already exists
+        # "replace, if table already exists
+        if_exists="append")
+
+utils.print_table(engine, "user")
